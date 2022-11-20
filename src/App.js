833f0153dee54078bbc0e79a8cfbe2371f423a4a
stevepetebruce <stevepetebruce@hotmail.com>
2022-11-20 17:54:03 +0000

    Fetch image custom hook

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import Tasks from "./components/Tasks";
 import TaskInput from "./components/TaskInput";
 import User from "./components/User";
 import TasksRemoveAll from "./components/TasksRemoveAll";
+import useFetchImage from "./hooks/useFetchImage";
 
 function App() {
+	const { loading, image } = useFetchImage({
+		url: "https://source.unsplash.com/random/800x600/?mars",
+	});
 	return (
 		<div className='App'>
+			{loading ? <p>Loading...</p> : <img src={image} alt='Mars' />}
+
 			<TaskInput />
 			<User />
 			<Tasks />
---
+++ src/hooks/useFetchImage.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+function useFetchImage({ url }) {
+	const [image, setImage] = useState(null);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		fetch(`${url}`)
+			.then((response) => response.blob())
+			.then((blob) => {
+				setImage(URL.createObjectURL(blob));
+				setLoading(false);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}, []);
+
+	return { image, loading };
+}
+
+export default useFetchImage;
