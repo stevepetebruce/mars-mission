886bfe4921ddcb00621871f3dfebd2d836f05c0d
stevepetebruce <stevepetebruce@hotmail.com>
2022-11-27 18:02:45 +0000

    App test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 import Tasks from "./components/Tasks";
 import TaskInput from "./components/TaskInput";
 import User from "./components/User";
+import UserInput from "./components/UserInput";
 import TasksRemoveAll from "./components/TasksRemoveAll";
+import TasksTotal from "./components/TasksTotal";
 import useFetchImage from "./hooks/useFetchImage";
 
 function App() {
@@ -11,13 +13,34 @@ function App() {
 		url: "https://source.unsplash.com/random/800x600/?mars",
 	});
 	return (
-		<div className='App'>
-			{loading ? <p>Loading...</p> : <img src={image} alt='Mars' />}
-
-			<TaskInput />
-			<User />
-			<Tasks />
-			<TasksRemoveAll />
+		<div className='container' data-testid='app'>
+			<header className='header'>
+				I would like to die on Mars. Just not on impact.
+			</header>
+			<main className='content-main'>
+				<aside className='content-left'>
+					<div className='card'>
+						<User />
+						<TasksTotal />
+						<TasksRemoveAll />
+					</div>
+				</aside>
+				<article className='content-right'>
+					<div className='article-wrapper'>
+						<div className='image-container'>
+							{loading ? (
+								<div className='spinner'></div>
+							) : (
+								<img src={image} alt='Mars' />
+							)}
+						</div>
+						<hr />
+						<UserInput />
+						<TaskInput />
+						<Tasks />
+					</div>
+				</article>
+			</main>
 		</div>
 	);
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./reducers/reducers";
+const store = createStore(rootReducer);
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+	test("renders App component", () => {
+		render(
+			<Provider store={store}>
+				<App />
+			</Provider>
+		);
+		const app = screen.getByTestId("app");
+		expect(app).toBeInTheDocument();
+	});
 });
